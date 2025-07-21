6f5d91060ce2d7804dd293cb1b72e1bddd7e0c0c
Taha Rauf <rauftaha757@gmail.com>
2025-07-21 22:50:46 +0500

    hello

--- lib/Screens/AuthScreens/registerpage.dart
+++ lib/Screens/AuthScreens/registerpage.dart
@@ -41,180 +41,190 @@ class _RegisterPageState extends State<RegisterPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
-                height: 70.h,
+                height: 34.h,
               ),
-              customText(
-                text: "Sign Up & Shine 🌟",
-                textAlign: TextAlign.right,
-                color: Colors.white, // or any color you want
-                fontsize: 26,        // adjust size as needed
-                fontWeight: FontWeight.bold,
-              ),
-
-
-              customText(
-                text: "Sign up to get started on your fitness journey!",
-                color: Colors.grey,
-                fontsize: 14,
-                fontWeight: FontWeight.w400,
-                textAlign: TextAlign.start,
-              ),
-              SizedBox(
-                height: 30.h,
-              ),
-              Center(
-          
-              ),
-            Container(
-              width: 320.w,
-              height: 450.h,
-          
-              decoration:
-              BoxDecoration(
-                color: Colors.grey.shade100, // Light grey box background
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  // Top-left shadow (orange instead of pink)
-                  BoxShadow(
-                    color: Color(0xFFF89B29).withOpacity(0.35),
-                    blurRadius: 12,
-                    spreadRadius: 1,
-                    offset: Offset(-6, -6),
-                  ),
-                  // Top-right shadow (pink instead of orange)
-                  BoxShadow(
-                    color: Color(0xFFFF0F7B).withOpacity(0.35),
-                    blurRadius: 12,
-                    spreadRadius: 1,
-                    offset: Offset(6, -6),
-                  ),
-                  // Bottom-left shadow (pink instead of orange)
-                  BoxShadow(
-                    color: Color(0xFFFF0F7B).withOpacity(0.35),
-                    blurRadius: 12,
-                    spreadRadius: 1,
-                    offset: Offset(-6, 6),
-                  ),
-                  // Bottom-right shadow (orange instead of pink)
-                  BoxShadow(
-                    color: Color(0xFFF89B29).withOpacity(0.35),
-                    blurRadius: 12,
-                    spreadRadius: 1,
-                    offset: Offset(12, 12),
-                  ),
-                ],
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.r),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    customText(
+                      text: "Let's get you ",
+                      color: Colors.white,
+                      fontsize: 40,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 2.sp,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 4.h),
+                    customText(
+                      text: "Signed In ",
+                      color: Colors.white,
+                      fontsize: 30,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 2.sp,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
               ),
 
 
-              child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      gradient: const LinearGradient(
-                        colors: [
-                          Color(0xFF0F2027), // deep navy
-                          Color(0xFF203A43), // blue-gray
-                          Color(0xFF2C5364), // cool navy blue
-                        ],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.blueGrey.withOpacity(0.6),
-                          spreadRadius: 4,
-                          blurRadius: 15,
-                          offset: Offset(0, 6),
+
+
+
+
+              Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Container(
+                width: double.infinity,
+                height: 490.h,
+
+                decoration:
+                BoxDecoration(
+                  color: Colors.grey.shade100, // Light grey box background
+                  borderRadius: BorderRadius.circular(21.r),
+                ),
+
+                child:Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color(0xFF0F2027),
+                                Color(0xFF203A43),
+                                Color(0xFF2C5364),
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.blueGrey.withOpacity(0.6),
+                                spreadRadius: 4,
+                                blurRadius: 15,
+                                offset: Offset(0, 6),
+                              ),
+                            ],
+                          ),
+                          child: CircleAvatar(
+                            radius: 50.r,
+                            backgroundColor: Colors.transparent,
+                            child: ClipOval(
+                              child: Image.asset(
+                                "assets/images/avatar2.png",
+                                width: 70.r,
+                                height: 100.r,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        // Pencil icon positioned at bottom right
+                        Positioned(
+                          bottom: 4,
+                          right: 4,
+                          child: Container(
+                            padding: EdgeInsets.all(4),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  blurRadius: 4,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: Icon(
+                              Icons.edit,
+                              size: 18.r,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
                         ),
                       ],
                     ),
-                    child: CircleAvatar(
-                      radius: 50.r,
-                      backgroundColor: Colors.transparent, // avoid white ring
-                      child: ClipOval(
-                        child: Image.asset(
-                          "assets/images/avatar2.png",
-                          width: 70.r,
-                          height: 100.r,
-                          fit:BoxFit.cover,
+
+
+                    Padding(
+                      padding:  EdgeInsets.only(top: 25.r),
+                      child: CustomTextFields(controller: namecontroller, obsecureText: false, hinttext: "Enter Name", fillcolor: Colors.grey.withOpacity(0.2), height: 50, width: 300, keyboardtype: TextInputType.text,prefixicon: Icon(Icons.person,color: Colors.black,),),
+                    ),
+                    Padding(
+                      padding:  EdgeInsets.only(top: 8.r),
+                      child: CustomTextFields(controller: emailcontroller, obsecureText: false, hinttext: "Enter Email", fillcolor:  Colors.grey.withOpacity(0.2), height: 50, width: 300, keyboardtype: TextInputType.text,prefixicon: Icon(Icons.mail,color: Colors.black,),),
+                    ),
+
+                    Padding(
+                      padding: EdgeInsets.only(top: 8.r),
+                      child: CustomTextFields(
+                        controller: usernamecontroller,
+                        obsecureText: false,
+                        hinttext: "Enter Username",
+                        fillcolor: Colors.grey.withOpacity(0.2),
+                        height: 50,
+                        width: 300,
+                        keyboardtype: TextInputType.text,
+                        prefixicon:  Padding(
+                          padding: EdgeInsets.all(10.r),
+                          child: Image.asset(
+                            "assets/images/at.png",
+                            width: 10.w,
+                            height: 10.h,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    )
+                        ,
+                    Padding(
+                      padding: EdgeInsets.only(top: 8.r),
+                      child: CustomTextFields(
+                        controller: passwordcontroller,
+                        obsecureText: obsec,
+                        hinttext: "Enter Password",
+                        fillcolor: Colors.grey.withOpacity(0.2),
+                        height: 50,
+                        width: 300,
+                        keyboardtype: TextInputType.text,
+                        prefixicon: Icon(Icons.lock, color: Colors.black),
+                        suffix: InkWell(
+                          onTap: () {
+                            setState(() {
+                              obsec = !obsec;
+                            });
+                          },
+                          child: Icon(
+                            obsec ? Icons.visibility_off : Icons.visibility,
+                            color: Colors.black,
+                          ),
                         ),
                       ),
                     ),
-                  )
-          
-                  ,
-                  Padding(
-                    padding:  EdgeInsets.only(top: 25.r),
-                    child: CustomTextFields(controller: namecontroller, obsecureText: false, hinttext: "Enter Name", fillcolor: Colors.grey.withOpacity(0.2), height: 50, width: 300, keyboardtype: TextInputType.text,prefixicon: Icon(Icons.person,color: Colors.black,),),
-                  ),
-                  Padding(
-                    padding:  EdgeInsets.only(top: 8.r),
-                    child: CustomTextFields(controller: emailcontroller, obsecureText: false, hinttext: "Enter Email", fillcolor:  Colors.grey.withOpacity(0.2), height: 50, width: 300, keyboardtype: TextInputType.text,prefixicon: Icon(Icons.mail,color: Colors.black,),),
-                  ),
-          
-                  Padding(
-                    padding: EdgeInsets.only(top: 8.r),
-                    child: CustomTextFields(
-                      controller: usernamecontroller,
-                      obsecureText: false,
-                      hinttext: "Enter Username",
-                      fillcolor: Colors.grey.withOpacity(0.2),
-                      height: 50,
-                      width: 300,
-                      keyboardtype: TextInputType.text,
-                      prefixicon:  Padding(
-                        padding: EdgeInsets.all(10.r),
-                        child: Image.asset(
-                          "assets/images/at.png",
-                          width: 10.w,
-                          height: 10.h,
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  )
-          ,
-                  Padding(
-                    padding: EdgeInsets.only(top: 8.r),
-                    child: CustomTextFields(
-                      controller: passwordcontroller,
-                      obsecureText: obsec,
-                      hinttext: "Enter Password",
-                      fillcolor: Colors.grey.withOpacity(0.2),
-                      height: 50,
-                      width: 300,
-                      keyboardtype: TextInputType.text,
-                      prefixicon: Icon(Icons.lock, color: Colors.black),
-                      suffix: InkWell(
-                        onTap: () {
+                    Padding(
+                      padding:  EdgeInsets.all(35.0),
+                      child: AnimatedContainer(
+                        width: ispressed ? 70.w : 250.w,
+                        height: 50.h,
+                        duration: Duration(milliseconds: 200),
+                        child: CustomButton(text:ispressed? "" : "Register", buttoncolor: Color(0xFF000000), textcolor: Colors.white, fontWeight: FontWeight.bold,  borderRadius: BorderRadius.circular(ispressed ? 100.r : 15.r), onpressed: (){
                           setState(() {
-                            obsec = !obsec;
+                            ispressed=!ispressed;
                           });
-                        },
-                        child: Icon(
-                          obsec ? Icons.visibility_off : Icons.visibility,
-                          color: Colors.black,
-                        ),
+                        }, fontsize: 21, elevation: 6,shadowcolor: Colors.grey,),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding:  EdgeInsets.all(35.0),
-                    child: AnimatedContainer(
-                      width: ispressed ? 70.w : 250.w,
-                      height: 45.h,
-                      duration: Duration(milliseconds: 200),
-                      child: CustomButton(text:ispressed? "" : "Register", buttoncolor: Color(0xFF000000), textcolor: Colors.white, fontWeight: FontWeight.bold,  borderRadius: BorderRadius.circular(ispressed ? 110.r : 30.r), onpressed: (){
-                        setState(() {
-                          ispressed=!ispressed;
-                        });
-                      }, fontsize: 21, elevation: 6,shadowcolor: Colors.grey,),
-                    ),
-                  )
-          
-                ],
+                    )
+
+                  ],
+                ),
               ),
             )
           
