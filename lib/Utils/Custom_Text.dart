a536a6d2f3fb4381dca317a30bc6e937beb1e40a
Taha Rauf <rauftaha757@gmail.com>
2025-07-21 22:51:12 +0500

    hello

--- lib/Utils/Custom_Text.dart
+++ lib/Utils/Custom_Text.dart
@@ -1,26 +1,35 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class customText extends StatelessWidget {
   final TextAlign? textAlign;
-      final String text;
-      final Color color;
-      final double fontsize;
-      final FontWeight fontWeight;
-      customText({
-        Key ?key ,
-        this.textAlign,
- required this.text, required this.color, required this.fontsize, required this.fontWeight})
-:super(key:key);
+  final String text;
+  final Color color;
+  final double fontsize;
+  final FontWeight fontWeight;
+  final double? letterSpacing;
+
+  customText({
+    Key? key,
+    this.textAlign,
+    required this.text,
+    required this.color,
+    required this.fontsize,
+    required this.fontWeight,
+    this.letterSpacing,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(text,textAlign: textAlign,style: TextStyle(
-      color: color,fontSize: fontsize.sp,fontWeight: fontWeight,
-
-    ),);
+    return Text(
+      text,
+      textAlign: textAlign,
+      style: TextStyle(
+        color: color,
+        fontSize: fontsize.sp,
+        fontWeight: fontWeight,
+        letterSpacing: letterSpacing,
+      ),
+    );
   }
-
 }
-
