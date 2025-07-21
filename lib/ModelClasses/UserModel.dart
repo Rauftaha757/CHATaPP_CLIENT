4c120d4c60efd09692ebc1a0852b478163f61a3d
Taha Rauf <rauftaha757@gmail.com>
2025-07-21 22:47:54 +0500

    hello

---
+++ lib/ModelClasses/UserModel.dart
@@ -0,0 +1,24 @@
+// import 'dart:io';
+//
+// class UserModel {
+//   final String name;
+//   final String email;
+//   final String? username;
+//   final String? password;
+//   final File avatar;
+//
+//   UserModel({
+//     required this.name,
+//     required this.email,
+//     this.username,
+//     this.password,
+//     required this.avatar,
+//   });
+//
+//   //tojson
+//
+// Map<String,dynamic> tojson(){
+//   return
+// }
+//
+// }
