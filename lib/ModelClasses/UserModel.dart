6ef325e2ae766b1bd626412ddeed58682cb56ab7
Taha Rauf <rauftaha757@gmail.com>
2025-07-23 04:35:57 +0500

    new files

--- lib/ModelClasses/UserModel.dart
+++ lib/ModelClasses/UserModel.dart
@@ -1,24 +1,37 @@
-// import 'dart:io';
-//
-// class UserModel {
-//   final String name;
-//   final String email;
-//   final String? username;
-//   final String? password;
-//   final File avatar;
-//
-//   UserModel({
-//     required this.name,
-//     required this.email,
-//     this.username,
-//     this.password,
-//     required this.avatar,
-//   });
-//
-//   //tojson
-//
-// Map<String,dynamic> tojson(){
-//   return
-// }
-//
-// }
+import 'dart:io';
+
+class UserModel {
+  final String ?name;
+  final String email;
+  final String? username;
+  final String? password;
+  final File? avatarFile;
+  final String? avatar;
+
+  UserModel({
+     this.name,
+    required this.email,
+    this.username,
+    this.password,
+    this.avatarFile,
+    this.avatar,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "name": name,
+      "email": email,
+      "username": username,
+      "password": password,
+    };
+  }
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      name: json['name'],
+      email: json['email'],
+      username: json['username'],
+      avatar: json['avatar'],
+    );
+  }
+}
